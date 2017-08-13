0473208fc9d61679a071a7c192d8fdb75ff8d4bb
Jose <josealfredocontreras92@gmail.com>
2017-08-13 18:04:06 -0500

    dos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#User.update( email: 'josealfredocontreras@gmail.com',role: 'Maneger')
-Event.create!(title:"Evento de ayer", start: Time.now - 1.day)
-Event.create!(title:"Evento de hoy", start: Time.now)
-Event.create!(title:"Evento de mañana", start: Time.now + 1.day)
-Event.create!(title:"Evento de la semana", start: Time.now - 3.day, end: Time.now + 3.day)
+User.update( email: 'josealfredocontreras@gmail.com',role: 'Maneger')
+#Event.create!(title:"Evento de ayer", start: Time.now - 1.day)
+#Event.create!(title:"Evento de hoy", start: Time.now)
+#Event.create!(title:"Evento de mañana", start: Time.now + 1.day)
+#Event.create!(title:"Evento de la semana", start: Time.now - 3.day, end: Time.now + 3.day)
