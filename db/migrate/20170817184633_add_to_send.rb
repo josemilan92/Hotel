acea02b2a82a0e2ef2ce86819a5f25520ea558e3
Jose <josealfredocontreras92@gmail.com>
2017-08-17 15:18:12 -0500

    dos

--- db/migrate/20170817184633_add_to_send.rb
+++ db/migrate/20170817184633_add_to_send.rb
@@ -3,10 +3,10 @@ class AddToSend < ActiveRecord::Migration[5.1]
     #add_column :sends, :pago, :string
     #add_column :sends, :total, :string
     #add_column :sends, :estatus, :integer
-    add_column :sends, :photo, :string
+    #add_column :sends, :photo, :string
     #add_column :sendpros, :pago, :string
     #add_column :sendpros, :total, :integer
     #add_column :sendpros, :status, :integer
-    add_column :sendpros, :photo, :string
+    #add_column :sendpros, :photo, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814231614) do
+ActiveRecord::Schema.define(version: 20170817184633) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
