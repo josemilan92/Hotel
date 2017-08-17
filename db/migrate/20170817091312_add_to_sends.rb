a6ff9b97568c957bec3aaa83a05b2f7c77fc7150
Jose <josealfredocontreras92@gmail.com>
2017-08-17 04:20:01 -0500

    cutro

--- db/migrate/20170817091312_add_to_sends.rb
+++
@@ -1,7 +0,0 @@
-class AddToSends < ActiveRecord::Migration[5.1]
-  def change
-    add_column :sends, :pago, :string
-    add_column :sends, :total, :string
-    add_column :sends, :status, :string
-  end
-end
