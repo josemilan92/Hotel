2ea2787d22154b817f514eae382ca953a87da672
Jose <josealfredocontreras92@gmail.com>
2017-08-17 14:32:36 -0500

    uno

---
+++ db/migrate/20170817184633_add_to_send.rb
@@ -0,0 +1,12 @@
+class AddToSend < ActiveRecord::Migration[5.1]
+  def change
+    #add_column :sends, :pago, :string
+    #add_column :sends, :total, :string
+    #add_column :sends, :estatus, :integer
+    add_column :sends, :photo, :string
+    #add_column :sendpros, :pago, :string
+    #add_column :sendpros, :total, :integer
+    #add_column :sendpros, :status, :integer
+    add_column :sendpros, :photo, :string
+  end
+end
