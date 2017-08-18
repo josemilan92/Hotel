8dddcec4f18c561df6ef3af5ce5bcfb77ffbab56
Jose <josealfredocontreras92@gmail.com>
2017-08-18 03:49:25 -0500

    tres

--- app/models/sendpro.rb
+++ app/models/sendpro.rb
@@ -2,5 +2,4 @@ class Sendpro < ApplicationRecord
   validates :start_time, uniqueness: { scope: :lugar , notice: "Fecha Ya utilizada" }
   validates :end_time, uniqueness: { scope: :lugar, notice:"Fecha no valida"}
 
-  attr_accessor     :lugar, :pago, :total, :status, :user_id, :photo, :promotion_id
 end
