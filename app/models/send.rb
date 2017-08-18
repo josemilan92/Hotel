fb115d8f673a381c792fd6f50aa2aae3018cdd91
Jose <josealfredocontreras92@gmail.com>
2017-08-17 22:38:12 -0500

    dos

--- app/models/send.rb
+++ app/models/send.rb
@@ -1,9 +1,5 @@
 class Send < ApplicationRecord
   has_many :hotels
-  belongs_to :hotels
-  belongs_to :categories
-  belongs_to :estados
-  belongs_to :pagos
   validates :start_time, uniqueness: { scope: :lugar , notice: "Fecha Ya utilizada" }
   validates :end_time, uniqueness: { scope: :lugar, notice:"Fecha no valida"}
 
