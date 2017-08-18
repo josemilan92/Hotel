5ae4e7f37537e2685f7417d901c690daf9e5e268
Jose <josealfredocontreras92@gmail.com>
2017-08-18 03:58:28 -0500

    tres

--- db/migrate/20170818085247_add_to_send_pro.rb
+++ db/migrate/20170818085247_add_to_send_pro.rb
@@ -1,6 +1,5 @@
 class AddToSendPro < ActiveRecord::Migration[5.1]
   def change
-    add_column :sendpros, :total, :integer
     add_column :sendpros, :status, :integer
     #add_column :sendpros, :photo, :string
   end
