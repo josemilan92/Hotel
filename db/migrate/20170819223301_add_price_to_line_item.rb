d6f3eb851b379456002474693fe920e457348325
Jose <josealfredocontreras92@gmail.com>
2017-08-22 21:27:44 -0500

    dos

--- db/migrate/20170819223301_add_price_to_line_item.rb
+++
@@ -1,8 +0,0 @@
-class AddPriceToLineItem < ActiveRecord::Migration[5.1]
-  def change
-    add_column :line_items, :price, :decimal
-    LineItem.all.each do |li|
-      li.price = li.product.price
-   end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+git remote rm heroku# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#User.update( email: 'josealfredocontreras@gmail.com',role: 'Maneger')
+User.update( email: 'josealfredocontreras@gmail.com',role: 'Maneger')
 #Event.create!(title:"Evento de ayer", start: Time.now - 1.day)
 #Event.create!(title:"Evento de hoy", start: Time.now)
 #Event.create!(title:"Evento de mañana", start: Time.now + 1.day)
 #Event.create!(title:"Evento de la semana", start: Time.now - 3.day, end: Time.now + 3.day)
-Estado.create!(estados:1, nombre:'Reservado')
-Estado.create!(estados:2, nombre:'En uso')
+#Estado.create!(estados:1, nombre:'Reservado')
+#Estado.create!(estados:2, nombre:'En uso')
